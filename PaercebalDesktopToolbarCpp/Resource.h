ee08416c3d464fc3e744644f6d0ad54549b7466c
Raoul Borges <paercebal@paercebal.org>
2023-03-26 16:02:24 +0200

    Add menu actions

--- PaercebalDesktopToolbarCpp/Resource.h
+++ PaercebalDesktopToolbarCpp/Resource.h
@@ -13,6 +13,8 @@
 #define IDI_SMALL				108
 #define IDC_PAERCEBALDESKTOPTOOLBARCPP			109
 #define IDC_MYICON				2
+#define ID_TOOLBAR_REGISTER             32771
+#define ID_TOOLBAR_UNREGISTER           32772
 #ifndef IDC_STATIC
 #define IDC_STATIC				-1
 #endif
@@ -23,7 +25,7 @@
 
 #define _APS_NO_MFC					130
 #define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_COMMAND_VALUE		32773
 #define _APS_NEXT_CONTROL_VALUE		1000
 #define _APS_NEXT_SYMED_VALUE		110
 #endif
